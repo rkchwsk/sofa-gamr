f682680a1a66fa385dcbd91414aaf4bd71ae1a77
Anton Rykachevskii <anton.rykachevskiy@gmail.com>
2024-07-13 23:50:36 +0200

    flute

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
 
 // Basic Three.js setup
 const scene = new THREE.Scene();
@@ -13,30 +16,32 @@ document.body.appendChild(renderer.domElement);
 
 const worldSize = 500;
 const planeGeometry = new THREE.PlaneGeometry(worldSize, worldSize);
-const planeMaterial = new THREE.MeshStandardMaterial({ color: 0x303010 });
+const planeMaterial = new THREE.MeshStandardMaterial({ color: 0x403000 });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.rotation.x = -Math.PI / 2;
 plane.position.y = -1;
 scene.add(plane);
 
 
-// const wGeometry = new THREE.SphereGeometry(10);
-// const wMaterial = new THREE.MeshStandardMaterial({color: 0x0000ff });
-// const w = new THREE.Mesh(wGeometry, wMaterial);
-// scene.add(w);
+const ikeaGeometry = new THREE.BoxGeometry(100,30,100);
+const ikeaMaterial = new THREE.MeshStandardMaterial({color: 0x0000ff });
+const ikea = new THREE.Mesh(ikeaGeometry, ikeaMaterial);
+ikea.position.z = 250;
+scene.add(ikea);
 
-const music = new Audio('FSODF.mp3')
+const music = new Audio('FSODFflute.mp3')
 music.loop = true
 music.play()
 
 //TREES -------------------------------------------------------------------------------
 
-const trees = [];
-const treesBoundingBoxes = [];
+//const trees = [];
+//const treesBoundingBoxes = [];
 
 function addTree(z, x) {
-    const trunkGeometry = new THREE.BoxGeometry(1,30,1);
-    const trunkMaterial = new THREE.MeshStandardMaterial({color: 0x220100 });
+    //const trunkGeometry = new THREE.BoxGeometry(1,30,1);
+    const trunkGeometry = new THREE.CylinderGeometry(0.3,1,30,10);
+    const trunkMaterial = new THREE.MeshStandardMaterial({color: 0x190100 });
     const trunk = new THREE.Mesh(trunkGeometry, trunkMaterial);
     trunk.position.x = x
     trunk.position.z = z 
@@ -51,48 +56,21 @@ function addTree(z, x) {
     leaves.position.y = 28;
     scene.add(leaves);
 
-    trees.push(trunk);
+    return trunk;
 }
 
-function addMEGATree(z, x) {
-    const trunkGeometry = new THREE.BoxGeometry(1,5,1);
-    const trunkMaterial = new THREE.MeshStandardMaterial({color: 0x021000 });
-    const trunk = new THREE.Mesh(trunkGeometry, trunkMaterial);
-    trunk.position.x = x
-    trunk.position.z = z 
-    trunk.position.y = 0;
-    scene.add(trunk);
-
-    const leavesGeometry = new THREE.BoxGeometry(7,2 + Math.random() * 20,7);
-    const leavesMaterial = new THREE.MeshStandardMaterial({ color: 0x119900 });
-    const leaves = new THREE.Mesh(leavesGeometry, leavesMaterial);
-    leaves.position.x = x
-    leaves.position.z = z 
-    leaves.position.y = 10;
-    scene.add(leaves);
-
-    trees.push(trunk);
-}
-
-
+var trees = []
 for (let i = - worldSize/2; i < worldSize/2; i++) {
+    trees[i + worldSize/2] = [];
     for (let j = -worldSize/2; j < worldSize/2; j++) {
-        if (Math.random() < 0.005 && (i != 0 && j != 0)) {
-            addTree(i, j);
+        if (Math.random() < 0.008 && (i != 0 && j != 0)) {
+            trees[i + worldSize/2][j + worldSize/2] = addTree(i, j);
+        } else {
+            trees[i + worldSize/2][j + worldSize/2] = -1;
         }
     } 
 } 
-/*
-addTree(0,1)
-addTree(6,7)
-addTree(-6,10)
-addTree(0,50)
-addTree(0,150)
-addMEGATree(0,250)
-*/
-console.log(trees.length);
-let cubeABox, cubeBBox;
-cubeABox = new THREE.Box3().setFromObject(trees[0]);
+
 //LIGHTS-------------------------------------------------------------------------------------------
 
 // Add a point light
@@ -109,20 +87,32 @@ const sofaMaterial = new THREE.MeshStandardMaterial({color: 0x441188 });
 const sofa = new THREE.Mesh(sofaGeometry, sofaMaterial);
 */
 
+
+
 const loader = new OBJLoader();
 let sofa;
 
-// load a resource
-loader.load(
-    'sofa.obj',
-    function (obj) {
-        sofa = obj;
-        sofa.position.set(0, -1, 0)
-        scene.add(sofa);
+const loaderGLTF = new GLTFLoader();
 
-        cubeBBox = new THREE.Box3().setFromObject(sofa);
-    }
-)
+loaderGLTF.load( './models/chair/chair.glb', function ( gltf ) {
+    gltf.scene.scale.set(0.015, 0.015, 0.015);
+    sofa = gltf.scene;
+    sofa.position.set(0, -1, 0);
+    scene.add( sofa );
+}, undefined, function ( error ) {
+    console.error( error );
+} );
+
+
+// loader.load(
+//     'sofa.obj',
+//     function (obj) {
+//         sofa = obj;
+//         sofa.position.set(0, -1, 0);
+//         scene.add(sofa);
+//     }
+// )
+
 
 
 
@@ -134,21 +124,51 @@ const speed = 0.02;
 
 var vz = 1
 var vx = 0
-const speedMultiplyier = 0.05
+const speedMultiplyier = 0.25
 const rotationSpeed = 0.05
 let keysPressed = {};   
 
 
 const leavesMp3 = new Audio('leaves.mp3');
+leavesMp3.volume = 0.5;
 
 
-function checkCollision(position) {
-    const sofaBox = new THREE.Box3().setFromObject(sofa);
+function checkCollision(position, i, j) {
+    const smallerSofa = sofa.clone();
+    //smallerSofa.scale.set(0.1, 0.1, 0.1);
+    const sofaBox = new THREE.Box3().setFromObject(smallerSofa);
     sofaBox.translate(position.clone().sub(sofa.position));
-    for (let tree of trees) {
-        const treeBox = new THREE.Box3().setFromObject(tree);
-        if (sofaBox.intersectsBox(treeBox)) {
-            return true;
+    var collisionCandidates = [];
+    const x = Math.round(i + worldSize/2);
+    const y = Math.round(j + worldSize/2);
+
+    //collisionCandidates.push(trees[x][y]);
+    for (let e = -1; e <= 1; e++) {
+        for (let k = -1; k <= 1; k++) {
+            if (x + e >= 0 && x + e < worldSize &&  y + k >= 0 && y + k < worldSize) {
+                collisionCandidates.push(trees[x + e][y + k]);
+            }
+        } 
+    }
+
+    console.log()
+
+    for (let tree of collisionCandidates) {
+        if (tree != -1) {
+            const treeBox = new THREE.Box3().setFromObject(tree);
+            console.log("tree ", tree);
+            console.log("sofa", sofaBox.getSize(), sofaBox.getCenter());
+            console.log("tree", treeBox.getSize(), treeBox.getCenter());
+
+            const sHelper = new THREE.Box3Helper(sofaBox, 0xffff00);
+            scene.add(shelper);
+            const tHelper = new THREE.Box3Helper(treeBox, 0xffff00);
+            scene.add(thelper);
+
+
+            if (sofaBox.intersectsBox(treeBox)) {
+                return true;
+            }
         }
     }
     return false;
@@ -158,11 +178,10 @@ function checkCollision(position) {
 function animate() {
     requestAnimationFrame(animate);
     if (keysPressed['ArrowUp']) {
-
         const newPosition = sofa.position.clone();
         newPosition.z -= vz * speedMultiplyier;
         newPosition.x -= vx * speedMultiplyier;
-            if (!checkCollision(newPosition)) {
+            if (!checkCollision(newPosition, newPosition.z, newPosition.x)) {
                 sofa.position.copy(newPosition);
                 camera.position.z -= vz * speedMultiplyier;
                 camera.position.x -= vx * speedMultiplyier;
@@ -171,10 +190,14 @@ function animate() {
         leavesMp3.play();
     }   
     if (keysPressed['ArrowDown']) { 
-        sofa.position.z += vz * speedMultiplyier;
-        sofa.position.x += vx * speedMultiplyier;
-        camera.position.z += vz * speedMultiplyier;
-        camera.position.x += vx * speedMultiplyier
+        const newPosition = sofa.position.clone();
+        newPosition.z += vz * speedMultiplyier;
+        newPosition.x += vx * speedMultiplyier;
+            if (!checkCollision(newPosition, newPosition.z, newPosition.x)) {
+                sofa.position.copy(newPosition);
+                camera.position.z += vz * speedMultiplyier;
+                camera.position.x += vx * speedMultiplyier;
+            }
         leavesMp3.play();
     }
     if (keysPressed['ArrowLeft']) {
